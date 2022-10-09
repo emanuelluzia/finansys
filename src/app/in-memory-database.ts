7d048cd9f70c81d1596390e9cb2e1273a261a869
Emanuel Luzia <emanuel.luzia@deliverit.com.br>
2022-10-09 11:47:56 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb(){
+        const categories = [
+          { id: 1, name: "Home", description: "House Bills" },
+          { id: 2, name: "Health", description: "Health Care and medicines" },
+          { id: 3, name: "Leisure", description: "Cinema, park, beach, etc" },
+          { id: 4, name: "Wage", description: "Salary of the month" },
+          { id: 5, name: "Freelas", description: "Freelancer Jobs" },
+        ];
+
+        return { categories }
+    }
+
+}
